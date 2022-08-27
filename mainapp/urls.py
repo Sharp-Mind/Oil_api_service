2ee8ec9241fd6f70d640359492bb7f9b13c976f2
Sharp_Mind <redlance988@gmail.com>
2022-08-27 19:54:22 +0300

    tests WIP

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from mainapp import views
 
-urlpatterns = [
-    # path("reports/report_info", views.ReportsListAPIView.as_view()),
+urlpatterns = [    
     path("api/v1/calculations", views.CalculationListAPIView.as_view(), name='calculations'),
     path("api/v1/calculations/<str:cid>", views.SingleCalculationListAPIView.as_view(), name='single_calculation'),
 ]
